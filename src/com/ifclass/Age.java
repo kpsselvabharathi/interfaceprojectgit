dd8242ecb3b8b15eef77e76379aba709dc3b3706
kpsselvabharathi <kpsselvabaharthi@gmail.com>
2022-02-19 21:16:05 +0530

    java

--- src/com/ifclass/Age.java
+++ src/com/ifclass/Age.java
@@ -3,7 +3,7 @@ package com.ifclass;
 public class Age{
 	public static void main(String[] args) {
 		int age=30;
-		if(age<18 && age>1)
+		if(age<18 && age>5)
 		{
 			System.out.println("not eligible to vote")	;	
 			}
