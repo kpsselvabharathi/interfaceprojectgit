7159ad7eb0518e4f67fe3f0a80ea3f97c5a1a381
kpsselvabharathi <kpsselvabaharthi@gmail.com>
2022-02-19 20:39:27 +0530

    message

---
+++ src/com/forloop/Sample.java
@@ -0,0 +1,18 @@
+package com.forloop;
+
+import java.util.Scanner;
+
+public class Sample {
+	 public static void main(String[] args) {
+		 Scanner scanner = new Scanner(System.in);
+		 int nextInt = scanner.nextInt();
+		 System.out.println(nextInt);
+		 if(nextInt%2==0)
+		 {
+			 System.out.println("This is even Number");
+		 }
+			 else
+				 System.out.println("this is odd number");
+		 }
+		//		 }
+	 }
---
+++ src/com/ifclass/Age.java
@@ -0,0 +1,19 @@
+package com.ifclass;
+
+public class Age{
+	public static void main(String[] args) {
+		int age=30;
+		if(age<18 && age>1)
+		{
+			System.out.println("not eligible to vote")	;	
+			}
+		else
+		{
+			System.out.println("eligible to voting");
+		}
+			
+	
+	}
+	
+
+}
